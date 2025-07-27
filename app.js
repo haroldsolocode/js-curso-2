fef15b1a962f6038c1a90f3ec7f753d25afad5f1
haroldsolocode <146598474+haroldsolocode@users.noreply.github.com>
2025-07-27 02:08:16 -0500

    Update app.js / sumaDosNumeros()

--- app.js
+++ app.js
@@ -25,3 +25,12 @@ function mostrarAlerta() {
 function mostarAlerta2(){
     alert("AMO JS");
 } 
+//Al hacer clic en el botón "suma", 
+//pide 2 números y muestra el resultado de la suma en una alerta.
+
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
